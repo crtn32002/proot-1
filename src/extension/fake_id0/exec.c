090d24140c70103f47e1fa8e58f437d5eca043a4
corbinlc <corbinlc@gmail.com>
2020-05-02 14:39:28 -0700

    Merge termux again (#21)
    * first pass at merging with termux master again
    
    * put back missing stat.h/.c files
    
    * small commit
    
    * clean up makefile
    
    * try to fix m32 loader build for arm
    
    * fix variable in makefile
    
    * different approach to getting correct compiler
    
    * small fix
    
    * more small fixes
    
    * abc
    
    * a

--- src/extension/fake_id0/exec.c
+++ src/extension/fake_id0/exec.c
@@ -6,12 +6,12 @@
 
 #include "extension/fake_id0/helper_functions.h"
 
-/** Handles execve system calls. Checks permissions in a meta file if it exists
- *  and returns errors matching those in execve(2).
+/** Handles execve system calls.
+ *  If setuid or setgid bits are set, just make ourselves root.
  */
 int handle_exec_enter_end(Tracee *tracee, Reg filename_sysarg, Config *config)
 {
-	int status, perms;
+	int status;
 	char path[PATH_MAX];
 	struct stat mode;
 
